d2da361d6c0bab2a25b24ca712bf322d23630127
avenirs05 <avenirs05@gmail.com>
2018-04-22 15:49:50 +0600

    first

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-﻿
+﻿<!doctype html>
+<html lang="ru">
+<head>
+    <title>Продай свое ООО</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">		
+
+    <!-- Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+
+    <!-- Css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Fav -->
+    <link rel="shortcut icon" href="/imgs/fav.png" type="image/png">
+</head>
+
+
+<body>	  
+test  
+    <!-- JS libs -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>  
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- My Own Js-scripts -->
+    <script src="js/main.js"></script>
+
+    <!-- Маска ввода -->
+    <script src="js/jquery.maskedinput.js" type="text/javascript"></script>
+
+</body>  
+
