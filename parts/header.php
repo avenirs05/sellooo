9dece414867cc8051ec7fa5381d6d2e2b28c8c67
avenirs05 <avenirs05@gmail.com>
2018-04-23 17:01:01 +0600

    last

--- parts/header.php
+++ parts/header.php
@@ -14,9 +14,7 @@
                     <span class="connects__email">info@sellooo.ru</span>
                     <img class="connects__icon connects__icon_telegram" src="image/connect/telegram.png" width="25" height="25" alt="">
                     <span class="connects__telegram-text">@sellooo.ru</span>
-                    <img class="connects__icon" src="image/connect/whatsapp.svg" width="27" height="27" alt="">
-                    
-                    <!-- <img class="connects__icon" src="image/connect/phone.svg" width="20" height="25" alt=""> -->
+                    <img class="connects__icon" src="image/connect/whatsapp.svg" width="27" height="27" alt="">                  
                     <span class="connects__phone-text">+7 967 063 16 46</span>
                 </div>
             </div>
@@ -28,7 +26,7 @@
 <div class="header container-fluid d-lg-none">
     <div class="row align-items-center">
         <div class="col d-flex align-items-center">
-            <a class="logo" href="#">
+            <a class="logo" href="/">
                 <img class="logo__img" src="image/logo.png" height="30" alt="">
             </a>
             <div class="header__title">Продай своё ООО</div>
@@ -37,7 +35,6 @@
     <div class="row">
         <div class="col connects d-flex align-items-center">
             <img class="connects__icon" src="image/connect/whatsapp.svg" width="27" height="27" alt="">            
-<!--             <img class="connects__icon" src="image/connect/phone.svg" width="20" height="25" alt=""> -->
             <span class="connects__phone-text">+7 967 063 16 46</span>
         </div>
     </div>
